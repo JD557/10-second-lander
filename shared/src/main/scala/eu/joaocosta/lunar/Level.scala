646f138ddabe94069059bce3b0cc0b6cf314539f
João Costa <jdpc557@gmail.com>
2023-04-16 10:48:56 +0200

    Variable dificulty

--- shared/src/main/scala/eu/joaocosta/lunar/Level.scala
+++ shared/src/main/scala/eu/joaocosta/lunar/Level.scala
@@ -10,21 +10,29 @@ final case class Level(
 
 object Level {
   def generate(number: Int, random: util.Random) = {
+    val dificultyMod = math.min(number / 4, 2).toInt
+
+    val padRange = 256 + 64 * dificultyMod
+
     val lowFreq      = 64 + random.nextInt(16)
     val lowFreqPhase = random.nextDouble() * 2 * math.Pi
+    val lowFreqAmp   = 30 + 10 * dificultyMod
     val highFreq     = 16 + random.nextInt(16)
-    val maxDelta     = 25
-    val constHeight  = 250
-    val minHeight    = constHeight - maxDelta
-    val maxHeight    = constHeight + maxDelta
+    val highFreqAmp  = 10 + 10 * dificultyMod
+
+    val maxDelta    = (lowFreqAmp + highFreqAmp) / 2 + 5
+    val constHeight = 250
+    val minHeight   = constHeight - maxDelta
+    val maxHeight   = constHeight + maxDelta
+
     val height = { (x: Double) =>
-      val raw = 30 * math.sin(x / lowFreq + lowFreqPhase) + 10 * math.sin(x / highFreq) + constHeight
+      val raw = lowFreqAmp * math.sin(x / lowFreq + lowFreqPhase) + highFreqAmp * math.sin(x / highFreq) + constHeight
       math.max(minHeight, math.min(raw, maxHeight))
     }
     val padX = {
       val candidates =
-        if (random.nextBoolean) (-256 to (256 - 32) by 8)
-        else (-256 to (256 - 32) by 8).reverse
+        if (random.nextBoolean) (-padRange to (padRange - 32) by 8)
+        else (-padRange to (padRange - 32) by 8).reverse
       candidates.minBy { x =>
         val thisHeight = (height(x) + height(x + 32)) / 2
         math.min(thisHeight - minHeight, maxHeight - thisHeight)
